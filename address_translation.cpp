020ba1ebb4a8e8e20deb71302871809ec83a0a47
pari-11 <parnikaa.thakur.2711@gmail.com>
2026-05-20 07:06:54 +0530

    Update address_translation.cpp

--- address_translation.cpp
+++ address_translation.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 
 int main() {
-    
+
+    // ---------------- PAGING ----------------
+
     int pageTable[10];
     int pages, pageSize;
-    
+
+    cout << "===== PAGING =====\n";
+
     cout << "Enter number of pages: ";
     cin >> pages;
 
@@ -25,20 +29,73 @@ int main() {
     cout << "Enter logical address: ";
     cin >> logicalAddress;
 
-    // Find page number and offset
+    // Find page number
     int pageNumber = logicalAddress / pageSize;
+
+    // Find offset
     int offset = logicalAddress % pageSize;
 
-    // Get frame number
+    // Get frame number from page table
     int frameNumber = pageTable[pageNumber];
 
     // Calculate physical address
     int physicalAddress = (frameNumber * pageSize) + offset;
 
-    cout << "\nPage Number = " << pageNumber << endl;
+    cout << "\n--- Paging Output ---\n";
+
+    cout << "Page Number = " << pageNumber << endl;
     cout << "Offset = " << offset << endl;
     cout << "Frame Number = " << frameNumber << endl;
     cout << "Physical Address = " << physicalAddress << endl;
+
+
+
+    // ---------------- SEGMENTATION ----------------
+
+    int segments;
+    int base[10], limit[10];
+
+    cout << "\n\n===== SEGMENTATION =====\n";
+
+    cout << "Enter number of segments: ";
+    cin >> segments;
+
+    // Input base and limit for each segment
+    for(int i = 0; i < segments; i++) {
+
+        cout << "\nSegment " << i << endl;
+
+        cout << "Enter base address: ";
+        cin >> base[i];
+
+        cout << "Enter limit: ";
+        cin >> limit[i];
+    }
+
+    int segmentNumber, segmentOffset;
+
+    cout << "\nEnter segment number: ";
+    cin >> segmentNumber;
+
+    cout << "Enter offset: ";
+    cin >> segmentOffset;
+
+    // Check whether offset is valid
+    if(segmentOffset < limit[segmentNumber]) {
+
+        // Physical address calculation
+        int physicalAddr = base[segmentNumber] + segmentOffset;
+
+        cout << "\n--- Segmentation Output ---\n";
+
+        cout << "Base Address = " << base[segmentNumber] << endl;
+        cout << "Offset = " << segmentOffset << endl;
+        cout << "Physical Address = " << physicalAddr << endl;
+    }
+    else {
+
+        cout << "\nSegmentation Fault! Offset exceeds limit.\n";
+    }
 
     return 0;
 }
